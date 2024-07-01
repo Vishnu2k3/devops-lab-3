e97ab335ca704ce086bc35b674dc84d26ff849f4
Vishnu K <164378061+Vishnu2k3@users.noreply.github.com>
2024-07-01 11:58:23 +0530

    Update and rename test.java to Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First");
+        System.out.println("Java");
+        System.out.println("Program.");
+    }
+};
